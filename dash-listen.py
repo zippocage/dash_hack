56476ad5712a6227ee15949e2929b2750f2ee24a
Klas Hermodsson <klas@hermodsson.se>
2015-12-24 14:47:10 +0100

    Fixed bug where non ARP packets were unpacked into ARP structs.

--- dash-listen.py
+++ dash-listen.py
@@ -73,12 +73,12 @@ while True:
     packet = rawSocket.recvfrom(2048)
     ethernet_header = packet[0][0:14]
     ethernet_detailed = struct.unpack("!6s6s2s", ethernet_header)
-    arp_header = packet[0][14:42]
-    arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
     # skip non-ARP packets
     ethertype = ethernet_detailed[2]
     if ethertype != '\x08\x06':
         continue
+    arp_header = packet[0][14:42]
+    arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", arp_header)
     source_mac = binascii.hexlify(arp_detailed[5])
     source_ip = socket.inet_ntoa(arp_detailed[6])
     dest_ip = socket.inet_ntoa(arp_detailed[8])
